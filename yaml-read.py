f7eb7656c2046a9fcc96b55bd295a7d3636faa98
Sandeep Reddy <107062578+imsandeepreddy@users.noreply.github.com>
2023-09-29 22:25:50 +0530

    Update yaml-read.py

--- yaml-read.py
+++ yaml-read.py
@@ -3,6 +3,6 @@ import yaml
 from yaml.loader import SafeLoader
 
 # Open the file and load the file
-with open('config.yaml') as f:
+with open('config.yml') as f:
     data = yaml.load(f, Loader=SafeLoader)
     print(data)
